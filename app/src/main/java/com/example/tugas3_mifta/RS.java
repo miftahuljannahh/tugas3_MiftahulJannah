f917866e1c4e6597ce8a0b4d87bb7764a7c50c42
miftahuljannahh <12050126869@students.uin-suska.ac.id>
2022-03-15 15:35:30 +0700

    1

---
+++ app/src/main/java/com/example/tugas3_mifta/RS.java
@@ -0,0 +1,43 @@
+package com.example.tugas3_mifta;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class RS extends ListActivity {
+    protected void onCreate (Bundle icicle) {
+        super.onCreate(icicle);
+        String[] listRS = new String[] {"Rumah Sakit Awal Bros","Rumah Sakit Aulia",
+                "Rumah Sakit Jiwa Tampan","Rumah Sakit Eka Hospital"};
+        this.setListAdapter(new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1,listRS));
+    }
+
+    protected void onListItemClick (ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        Object o = this.getListAdapter().getItem(position);
+        String pilihan = o.toString();
+        tampilkanpilihan(pilihan);
+    }
+
+    private void tampilkanpilihan(String pilihan) {
+        try {
+            Intent a = null;
+            if(pilihan.equals("Rumah Sakit Awal Bros")) {
+                a = new Intent(this, RSAwalBross.class);
+
+
+            } else if (pilihan.equals("Rumah Sakit Aulia")); {
+
+            }
+
+            startActivity(a);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
